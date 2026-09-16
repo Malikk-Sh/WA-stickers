9a4cf152ad1fc34b54898f80920ed7d7c723494d
Малик <93332098+Malikk-Sh@users.noreply.github.com>
2026-09-16 14:05:15 +0300

    Use AndroidX photo picker support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,16 @@ android {
         applicationId = "com.malikksh.wastickers"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
+
+dependencies {
+    implementation("androidx.activity:activity:1.10.1")
 }
