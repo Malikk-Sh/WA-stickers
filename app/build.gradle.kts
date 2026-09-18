bcfd57347a6b2c9a14a25d406b5130cdeb071c9b
Малик <93332098+Malikk-Sh@users.noreply.github.com>
2026-09-18 10:12:31 +0300

    Bump crash-fix test build to 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.malikksh.wastickers"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.5.1"
     }
 
     buildFeatures {
