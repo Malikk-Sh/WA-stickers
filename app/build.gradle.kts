4e1d5d8ae2a2854ebe428134f2c462f9df46e394
Малик <93332098+Malikk-Sh@users.noreply.github.com>
2026-09-18 16:38:25 +0300

    Bump optimized animated build to 1.5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.malikksh.wastickers"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.5.5"
+        versionCode = 12
+        versionName = "1.5.6"
     }
 
     buildFeatures {
