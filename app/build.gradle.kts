d7c5b903a7ad3bd66a4c51d0748ff5b212249bce
Малик <93332098+Malikk-Sh@users.noreply.github.com>
2026-09-19 17:36:27 +0300

    Release prep: WA Stickers 1.6.0 redesign (#38)
    * Prepare redesigned 1.6.0 release version
    
    * Document redesigned photo and animated sticker workflow
    
    * Add 1.6.0 redesign changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.malikksh.wastickers"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.5.7"
+        versionCode = 14
+        versionName = "1.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
